c1cb6df2cdeca9179e4dcf76324114fc5f6e3694
Johnathan Bailey <96433970+jjbailey-io@users.noreply.github.com>
2025-05-29 16:12:01 -0400

    Misc updates to allow a 1.x version of Foundry

--- script/python/get_selectors.py
+++ script/python/get_selectors.py
@@ -11,7 +11,7 @@ def get_selectors(args):
     line = ""
     while line == "":
         res = subprocess.run(
-            ["forge", "inspect", contract, "mi"], capture_output=True )
+            ["forge", "inspect", contract, "mi", "--json"], capture_output=True )
         line = res.stdout.decode()
     res = json.loads(line)
 
