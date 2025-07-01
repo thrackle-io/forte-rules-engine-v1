3269550e2b0316ee01cc30689d4b99ff0ea8cfb7
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2025-07-01 12:00:40 -0500

    Added verification parameters to creation commands

--- script/clientScripts/DeployForForteToken.sh
+++ script/clientScripts/DeployForForteToken.sh
@@ -1,6 +1,6 @@
 souce .env
-forge script script/clientScripts/Application_Deploy_01_AppManager.s.sol --ffi -vvvv --rpc-url $ETH_RPC_URL --broadcast --gas-price 20
+forge script script/clientScripts/Application_Deploy_01_AppManager.s.sol --ffi -vvvv --rpc-url $ETH_RPC_URL --broadcast --gas-price 20 --verify --verifier etherscan --etherscan-api-key $ETHERSCAN_API_KEY
 souce .env
-forge script script/clientScripts/DeployERC20Handler.s.sol --ffi -vvvv --rpc-url $ETH_RPC_URL --broadcast --gas-price 20
+forge script script/clientScripts/DeployERC20Handler.s.sol --ffi -vvvv --rpc-url $ETH_RPC_URL --broadcast --gas-price 20 --verify --verifier etherscan --etherscan-api-key $ETHERSCAN_API_KEY
 souce .env
-forge script script/clientScripts/DeployERC20HandlerPt2.s.sol --ffi -vvvv --rpc-url $ETH_RPC_URL --broadcast --gas-price 20
+forge script script/clientScripts/DeployERC20HandlerPt2.s.sol --ffi -vvvv --rpc-url $ETH_RPC_URL --broadcast --gas-price 20 --verify --verifier etherscan --etherscan-api-key $ETHERSCAN_API_KEY
