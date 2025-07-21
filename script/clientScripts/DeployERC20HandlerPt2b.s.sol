1ed6826881cb0f48f753b73d1208c44e8dfc182b
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2025-07-21 10:48:36 -0500

    Broke the handler part 2 up

---
+++ script/clientScripts/DeployERC20HandlerPt2b.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import "./DeployBase.s.sol";
+import {HandlerDiamond, HandlerDiamondArgs} from "src/client/token/handler/diamond/HandlerDiamond.sol";
+
+/**
+ * @title Deploy ERC20 Handler Diamond Script
+ * @dev This script will deploy the ERC20 Handler Diamons.
+ */
+contract DeployERC20HandlerPt1a is Script, DeployBase {
+    /// address and private key used to for deployment
+    uint256 privateKey;
+    address ownerAddress;
+    string name;
+    HandlerDiamond applicationCoinHandlerDiamond;
+
+    function run() external {
+        privateKey = vm.envUint("DEPLOYMENT_OWNER_KEY");
+        ownerAddress = vm.envAddress("DEPLOYMENT_OWNER");
+        name = vm.envString("RULE_PROCESSOR_DIAMOND"); // name of the Diamond
+        vm.startBroadcast(privateKey);
+        applicationCoinHandlerDiamond = HandlerDiamond(payable(vm.envAddress("APPLICATION_ERC20_HANDLER_ADDRESS")));
+
+        createERC20HandlerDiamondPt2b(name, address(applicationCoinHandlerDiamond));
+        // ERC20HandlerMainFacet(address(applicationCoinHandlerDiamond)).initialize(vm.envAddress("RULE_PROCESSOR_DIAMOND"), vm.envAddress("APPLICATION_APP_MANAGER"), vm.envAddress("APPLICATION_ERC20_ADDRESS"));
+        // vm.stopBroadcast();
+    }
+
+}
